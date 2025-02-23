0ab362a4b38d39da692e9124d535c29cd9949de3
윤준석(YunJunSeok) <wicaduce@gmail.com>
2025-02-23 16:22:01 +0900

    Fix: 괄호 열기 전 띄어쓰기 수정

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -7,12 +7,12 @@ public class ResultView {
         System.out.println("1000원 단위로 입력해주세요.");
     }
 
-    public static void printOrderTickets(int ticketCount){
+    public static void printOrderTickets(int ticketCount) {
         System.out.println();
         System.out.println(ticketCount + "개를 구매했습니다.");
     }
 
-    public static void printTickets(List<String> formattedTickets){
+    public static void printTickets(List<String> formattedTickets) {
         for (String ticket : formattedTickets) {
             System.out.println(ticket);
         }
